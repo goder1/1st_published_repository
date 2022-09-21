e3ac9c9ab7aa807bad022cddf55e69feb4a53f92
goder <goder_228@mail.ru>
2022-09-21 20:37:07 +0300

    Revert "Vibor partii added"
    This reverts commit f34697b81ad8c69446c6f2184c369c50e2d788a9.

--- main/main.cpp
+++ main/main.cpp
@@ -25,7 +25,7 @@ int main()
     fout<<a1<<" "<<a2<<" "<<a3;
     fout.close();
     }
-    else if(main_root == 2)result();
+    else if(main_root==2)result();
     else cout<<"-_-";
 
     return 0;
