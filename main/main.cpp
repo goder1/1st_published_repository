5a06796f2739db46a05673eaa39f74c05381b658
goder <goder_228@mail.ru>
2022-09-21 20:29:42 +0300

    Cpp file added

---
+++ main/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void result()
+{
+    string a1,a2,a3;
+    ifstream fin("Output.txt");
+    fin>>a1>>a2>>a3;
+    cout<<"pet_name:\tfavourite tv series:\t street name:\n";
+    cout<<a1<<"\t\t"<<a2<<"\t\t"<<a3;
+}
+int main()
+{
+    cout<<"Do you want to enter your answers or to get the previous Data? (1 or 2)?\n";
+    int main_root;
+    cin>>main_root;
+    if(main_root==1){
+    ofstream fout("Output.txt");
+    ifstream finq("Questionz.txt");
+    string q1, q2, q3, a1, a2, a3;
+    finq>>q1>>q2>>q3;
+    cout<<"Enter your "<<q1<<", "<<q2<<", "<<q3<<endl;
+    cin>>a1>>a2>>a3;
+    fout<<a1<<" "<<a2<<" "<<a3;
+    fout.close();
+    }
+    else if(main_root==2)result();
+    else cout<<"-_-";
+
+    return 0;
+}
